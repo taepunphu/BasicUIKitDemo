6b756128f0b1e850c66fb791bf758e77c352f896
Kiattisak Phanphu <43429886+taepunphu@users.noreply.github.com>
2023-12-23 11:19:57 +0700

    setup MVVM design pattern

---
+++ BasicUIKitDemo/Models/Manager.swift
@@ -0,0 +1,18 @@
+//
+//  Coach.swift
+//  BasicUIKitDemo
+//
+//  Created by NB1003517 on 23/12/2566 BE.
+//
+
+import Foundation
+
+enum JobType {
+    case manager
+    case headCoach
+}
+
+struct Manager {
+    let name: String
+    let job: JobType
+}
---
+++ BasicUIKitDemo/Models/Team.swift
@@ -0,0 +1,17 @@
+//
+//  Team.swift
+//  BasicUIKitDemo
+//
+//  Created by NB1003517 on 23/12/2566 BE.
+//
+
+import Foundation
+
+struct Team {
+    let id: TeamType
+    let name: String
+    let info: String
+    let manager: Manager
+    let founded: String
+    var isPlaying: Bool = false
+}
